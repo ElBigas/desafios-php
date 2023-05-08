222c9af1bda68c99b89ba4cc5ae698d8ef9758ce
Emanuel Evangelista <97486394+ElBigas@users.noreply.github.com>
2023-05-08 20:21:09 -0300

    Add files via upload

--- d05/index.php
+++ d05/index.php
@@ -5,34 +5,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="../../css/style.css">
 
     <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <title>Formulários com PHP</title>
+    <title>Desafio 5</title>
 </head>
 
 <body>
 
     <header class="mt-3 mb-3">
         <h1>
-            Faça sua apresentação
+            Analisador de número real
         </h1>
     </header>
 
-    <section>
+    <main>
+
         <form action="cad.php" method="get">
-            <label for="name" class="form-label">Nome: </label>
-            <input type="text" name="name" id="idName" placeholder="Nome">
 
-            <label for="lastName" class="form-label">Sobrenome: </label>
-            <input type="text" name="lastName" id="idLastName" placeholder="Sobrenome">
+            <label for="real" class="form-label">Número Real:</label>
+            <input type="number" name="real" id="idreal" step="0.001">
 
-
-            <button type="submit" class="btn btn-primary mb-3" id="btn-send">Enviar</button>
-
+            <button type="submit" class="btn btn-primary mb-3" id="btn-send">Analisar</button>
         </form>
-    </section>
+
+    </main>
 
 </body>
 
